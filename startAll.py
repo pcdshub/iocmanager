ab7e699de84ff4f795e2c6d531a2f74cbbac46fe
Zachary Lentz <zlentz@slac.stanford.edu>
2025-01-23 12:00:22 -0800

    MNT: manual ruff fixes for startAll.py

--- startAll.py
+++ startAll.py
@@ -7,14 +7,14 @@ if __name__ == "__main__":
     cfg = sys.argv[1]
     host = sys.argv[2]
     result = utils.readConfig(cfg)
-    if result == None:
+    if result is None:
         print("Cannot read configuration for %s!" % cfg)
         sys.exit(-1)
     (mtime, config, hosts, vdict) = result
-    for l in config:
-        if l["host"] == host and l["disable"] == False:
-            utils.startProc(cfg, l, True)
+    for ioc in config:
+        if ioc["host"] == host and not ioc["disable"]:
+            utils.startProc(cfg, ioc, True)
             try:
-                time.sleep(l["delay"])
-            except:
+                time.sleep(ioc["delay"])
+            except Exception:
                 pass
