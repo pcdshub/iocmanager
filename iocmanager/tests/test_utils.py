a682b2b4747e81e3ad733ffa0c543da26e8cce82
Zachary Lentz <zlentz@slac.stanford.edu>
2025-05-07 15:44:30 -0700

    TST: fix broken test_read_all from moving EPICS_SITE_TOP

--- iocmanager/tests/test_utils.py
+++ iocmanager/tests/test_utils.py
@@ -1004,18 +1004,16 @@ def test_check_ssh():
     assert not check_ssh("tstopr", "pytest")
 
 
-def test_read_all(tmp_path: Path):
+def test_read_all():
     # This is pretty dumb but whatever
     my_lines = [
         "hey\n",
         "this is an epics thing\n",
         "I guess\n",
     ]
-    with open(tmp_path / "test_read_all", "w") as fd:
-        fd.writelines(my_lines)
 
-    assert readAll("test_read_all") == my_lines
-    assert readAll(str(tmp_path / "test_read_all")) == my_lines
+    assert readAll("iocs/test_read_all.txt") == my_lines
+    assert readAll(str(TESTS_FOLDER / "iocs" / "test_read_all.txt")) == my_lines
     assert readAll("defo_not_a_path") == []
 
 
