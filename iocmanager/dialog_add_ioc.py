467f13c9fdf153b2cf0de4a3d8ace7e4f65b22e0
Zachary Lentz <zlentz@slac.stanford.edu>
2025-06-20 15:05:06 -0700

    FIX/TST: clean up test suite, fix bugs, and work around weird qt behavior in add_ioc dialog

--- iocmanager/dialog_add_ioc.py
+++ iocmanager/dialog_add_ioc.py
@@ -6,6 +6,8 @@ This dialog helps the user add an IOC to the GUI table.
 
 from __future__ import annotations
 
+import os
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 from qtpy.QtCore import QUrl
@@ -46,6 +48,7 @@ class AddIOCDialog(QFileDialog):
                 default_dir = pth
                 break
         super().__init__(parent, "Add New IOC", default_dir)
+        self.default_path = Path(default_dir)
         self.setFileMode(QFileDialog.Directory)
         self.setOptions(QFileDialog.ShowDirsOnly | QFileDialog.DontUseNativeDialog)
         self.setSidebarUrls(QUrl("file://" + pth) for pth in standard_paths)
@@ -73,9 +76,10 @@ class AddIOCDialog(QFileDialog):
         self.parent_edit.setReadOnly(True)
         self._add_row("* = Required Fields for Soft IOCs.")
         self._add_row("+ = Required fields for Hard IOCs.")
+        self.reset()
+        self.name_edit.textChanged.connect(self._update_parent)
         self.directoryEntered.connect(self._update_parent)
         self.currentChanged.connect(self._update_parent)
-        self.reset()
 
     def _add_row[T](self, text: str, widget: T = None) -> T:
         """Helper for adding a widget to the grid, similar to a form layout."""
@@ -137,11 +141,41 @@ class AddIOCDialog(QFileDialog):
         unused_port = self.model.get_unused_port(host=host, closed=closed)
         self.port_spinbox.setValue(unused_port)
 
-    def _update_parent(self, selected_path: str):
-        """Set the parent widget to the IOC parent (for templated IOCs)"""
+    def _update_parent(self, _: str):
+        """
+        Set the parent widget to the IOC parent (for templated IOCs)
+
+        The input variable may be any of the three texts we need,
+        ignore it and check all the values.
+        """
         ioc_name = self.name_edit.text().strip()
+        selected_path = self._get_selected_path()
         parent_path = self._get_parent(selected_path=selected_path, ioc_name=ioc_name)
         self.parent_edit.setText(parent_path)
+
+    def _get_selected_path(self) -> str:
+        """
+        Helper for dealing with the dumb stuff in QFileDialog.
+
+        It's really common to get into an invalid state just by clicking
+        normally. Handle the edge cases here to try our best to get the
+        correct directory.
+        """
+        try:
+            # Normal place if things work
+            selected_path = self.selectedFiles()[0].strip()
+        except KeyError:
+            # No file selected, use directory
+            selected_path = self.directory().path()
+        if os.path.exists(selected_path):
+            # Thank goodness
+            return selected_path
+        # Sometimes, you get weird stuff like repeating the dirname twice
+        # So try the parent
+        elif os.path.exists(os.path.dirname(selected_path)):
+            return os.path.dirname(selected_path)
+        # That's enough, let's move on
+        return selected_path
 
     def _get_parent(self, selected_path: str, ioc_name: str) -> str:
         """Inner wrapper for _update_parent that returns the str to use."""
@@ -161,6 +195,12 @@ class AddIOCDialog(QFileDialog):
         self.host_edit.setText("")
         self.port_spinbox.setValue(30001)
         self.parent_edit.setText("")
+        # Workaround for no direct way to clear selected path
+        # Move to a directory where we can select the default path as our dir
+        self.setDirectory(str(self.default_path.parent))
+        self.selectFile(str(self.default_path.name))
+        # Then, move into the (default) dir we selected
+        self.setDirectory(str(self.default_path))
 
     def get_ioc_proc(self) -> IOCProc:
         """
@@ -176,6 +216,6 @@ class AddIOCDialog(QFileDialog):
             name=name,
             port=port,
             host=host,
-            path=self.selectedFiles()[0].strip(),
+            path=self._get_selected_path(),
             alias=self.alias_edit.text().strip(),
         )
--- iocmanager/tests/test_dialog_add_ioc.py
+++ iocmanager/tests/test_dialog_add_ioc.py
@@ -5,8 +5,9 @@ import pytest
 from pytestqt.qtbot import QtBot
 from qtpy.QtCore import Qt
 from qtpy.QtGui import QColor
-from qtpy.QtWidgets import QMessageBox
+from qtpy.QtWidgets import QApplication, QMessageBox
 
+from ..config import Config
 from ..dialog_add_ioc import AddIOCDialog
 from ..table_model import IOCTableModel
 from . import IOC_FOLDER
@@ -20,11 +21,25 @@ def add_ioc_dialog(model: IOCTableModel, qtbot: QtBot) -> AddIOCDialog:
 
 
 def setup_cool_ioc(dialog: AddIOCDialog):
-    dialog.name_edit.setText("cool_name")
+    dialog.name_edit.setText("ioc_name")
     dialog.alias_edit.setText("CoolIOC")
     dialog.host_edit.setText("neat_host")
     dialog.port_spinbox.setValue(31000)
-    dialog.selectFile("/some/filename")
+    # A real directory different from the reset default of IOC_FOLDER / "pytest"
+    dialog.setDirectory(str(IOC_FOLDER / "hutch_name"))
+    # Note: selectFile is used to set the selected directory
+    # This is because the dialog is in directory mode
+    dialog.selectFile("repo_name")
+    # See tests/ioc/hutch_name/repo_name/ioc_name.cfg
+
+
+def run_interactive():
+    """Outside of test suite helper for opening the dialog in a clickable form."""
+    app = QApplication([])  # noqa: F841
+    config = Config("")
+    model = IOCTableModel(config=config, hutch="pytest")
+    dialog = AddIOCDialog(hutch="pytest", model=model, parent=None)
+    dialog.exec_()
 
 
 def test_get_ioc_proc(add_ioc_dialog: AddIOCDialog):
@@ -33,11 +48,11 @@ def test_get_ioc_proc(add_ioc_dialog: AddIOCDialog):
     """
     setup_cool_ioc(dialog=add_ioc_dialog)
     ioc_proc = add_ioc_dialog.get_ioc_proc()
-    assert ioc_proc.name == "cool_name"
+    assert ioc_proc.name == "ioc_name"
     assert ioc_proc.alias == "CoolIOC"
     assert ioc_proc.host == "neat_host"
     assert ioc_proc.port == 31000
-    assert ioc_proc.path == "/some/filename"
+    assert ioc_proc.path == str(IOC_FOLDER / "hutch_name" / "repo_name")
 
 
 def test_reset(add_ioc_dialog: AddIOCDialog):
@@ -51,21 +66,65 @@ def test_reset(add_ioc_dialog: AddIOCDialog):
     assert ioc_proc.alias == ""
     assert ioc_proc.host == ""
     assert ioc_proc.port == 30001
-    assert ioc_proc.path == ""
+    assert ioc_proc.path == str(IOC_FOLDER / "pytest")
 
 
-def test_parent(add_ioc_dialog: AddIOCDialog):
+@pytest.mark.parametrize("mode", ("name", "directory", "file"))
+def test_parent(mode: str, add_ioc_dialog: AddIOCDialog, qtbot: QtBot):
     """
     If we select a templated IOC, the parent text should be set correctly.
+
+    Test whether modulating the ioc name, directory, or file
+    properly triggers the update separately.
     """
-    # see ioc/templated_ioc/hutch_ioc.cfg
-    # note, text should update when name or folder changes
-    add_ioc_dialog.name_edit.setText("hutch_ioc")
-    assert add_ioc_dialog.parent_edit.text() == ""
-    add_ioc_dialog.selectFile(str(IOC_FOLDER / "templated_ioc"))
-    assert add_ioc_dialog.parent_edit.text() == "/some/absolute/path"
-    add_ioc_dialog.name_edit.setText("")
-    assert add_ioc_dialog.parent_edit.text() == ""
+
+    def correct_parent():
+        assert add_ioc_dialog.parent_edit.text() == "/test/dialog/add_ioc"
+
+    def no_parent():
+        assert add_ioc_dialog.parent_edit.text() == ""
+
+    # I couldn't find a way to trigger these signals in a test suite.
+    # Even running interactively they're pretty unreliable.
+    # This is a compromise.
+
+    def set_dir_and_emit(target: str):
+        add_ioc_dialog.setDirectory(target)
+        add_ioc_dialog.directoryEntered.emit(target)
+
+    def sel_file_and_emit(target: str):
+        add_ioc_dialog.selectFile(target)
+        try:
+            emission = add_ioc_dialog.selectedFiles()[0]
+        except KeyError:
+            emission = target
+        add_ioc_dialog.currentChanged.emit(emission)
+
+    setup_cool_ioc(dialog=add_ioc_dialog)
+
+    match mode:
+        case "name":
+            add_ioc_dialog.name_edit.setText("")
+        case "directory":
+            set_dir_and_emit(str(IOC_FOLDER))
+        case "file":
+            sel_file_and_emit("asdf")
+        case _:
+            raise ValueError("Error writing test")
+
+    qtbot.wait_until(no_parent, timeout=1000)
+
+    match mode:
+        case "name":
+            add_ioc_dialog.name_edit.setText("ioc_name")
+        case "directory":
+            set_dir_and_emit(str(IOC_FOLDER / "hutch_name"))
+        case "file":
+            sel_file_and_emit("repo_name")
+        case _:
+            raise ValueError("Error writing test")
+
+    qtbot.wait_until(correct_parent, timeout=1000)
 
 
 def test_port_select(
