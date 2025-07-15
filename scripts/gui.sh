c81e3963c3173e709d9e72037343be8cc090a607
Zachary Lentz <zlentz@slac.stanford.edu>
2025-07-15 11:48:06 -0700

    MNT: turn off QT_XCB_GL_INTEGRATION, spammy warnings in terminal

--- scripts/gui.sh
+++ scripts/gui.sh
@@ -3,4 +3,6 @@ THIS_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 cd "${THIS_DIR}/.." || exit
 source "${THIS_DIR}"/default_env
 
+export QT_XCB_GL_INTEGRATION=none
+
 "${IOCMAN_PY_BIN}"/python -m iocmanager "$@"
