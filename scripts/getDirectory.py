95b8a768f86ec0aee1980401d8aa1dd3df51bc11
Jane Liu <janeliu@slac.stanford.edu>
2026-03-04 11:14:07 -0800

    Applied pre-commit formatting changes to scripts/getDirectory.py.

--- scripts/getDirectory.py
+++ scripts/getDirectory.py
@@ -3,6 +3,7 @@ Backcompat script for dealing with old py2 iocmanager's startProc.
 
 Can remove when all instances of the py2 iocmanager are gone.
 """
+
 import subprocess
 import sys
 from pathlib import Path
@@ -12,4 +13,3 @@ REPO_ROOT = Path(__file__).parent.parent
 args = ["python", "-m", "iocmanager.scripts.get_directory"] + sys.argv[1:]
 
 subprocess.run(args, cwd=str(REPO_ROOT))
-
